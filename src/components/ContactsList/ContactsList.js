3a4c29c8cfe2abc2a182c201784e2b6ff12a1b7e
Ihor Poliakov <piv2003@gmail.com>
2023-08-02 18:19:03 +0200

    Update ContactsList.js

--- src/components/ContactsList/ContactsList.js
+++ src/components/ContactsList/ContactsList.js
@@ -1,6 +1,7 @@
-import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
 import ContactListItem from 'components/ContactListItem/ContactListItem';
 import { List } from './ContactsList.styled';
+import { getContacts, getFilter } from '../../redux/selectors';
 
 const ContactsList = ({ contacts, filter, onDeleteContact }) => {
   return (
