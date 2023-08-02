6c9db7a6d6382d398ce9e58bc2c8baea07edfcab
Ihor Poliakov <piv2003@gmail.com>
2023-08-02 18:10:38 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import { useDispatch, useSelector } from 'react-redux';
 import 'react-toastify/dist/ReactToastify.css';
 import { toast, ToastContainer } from 'react-toastify';
-import { AppBox } from './App.styled';
+import { AppBox, Empty } from './App.styled';
 import { getContacts } from '../redux/selectors';
 import { addContact } from '../redux/contactsSlice';
 import Form from './Form/Form';
@@ -12,18 +12,35 @@ export default function App() {
   const contacts = useSelector(getContacts);
   const dispatch = useDispatch();
 
-   function notifiesAlert(numberContact) {
-     return toast.error(`${numberContact} is already in contacts.`);
-     
-      function checkСontact(newNumber) {
+  function notifiesAlert(numberContact) {
+    return toast.error(`${numberContact} is already in contacts.`);
+  }
+
+  function checkСontact(newNumber) {
     return contacts.some(contact => contact.number === newNumber);
-     }
-     
-     function onSubmit(name, number) {
+  }
+
+  function onSubmit(name, number) {
     checkСontact(number)
       ? notifiesAlert(number)
       : dispatch(addContact(name, number));
   }
 
-  return <AppBox></AppBox>;
+  return (
+    <AppBox>
+      <ToastContainer autoClose={2000} position="top-center" />
+      <h1>Phonebook</h1>
+      <Form onSubmit={onSubmit} />
+
+      <h2>Contacts</h2>
+      {contacts !== undefined && contacts.length > 0 ? (
+        <>
+          <Filter />
+          <ContactsList />
+        </>
+      ) : (
+        <Empty>Contacts list is empty</Empty>
+      )}
+    </AppBox>
+  );
 }
