01b3be7f2a171abf2692804117ea3ce19d714dc6
Ihor Poliakov <piv2003@gmail.com>
2023-08-02 18:20:17 +0200

    Update ContactsList.js

--- src/components/ContactsList/ContactsList.js
+++ src/components/ContactsList/ContactsList.js
@@ -3,19 +3,16 @@ import ContactListItem from 'components/ContactListItem/ContactListItem';
 import { List } from './ContactsList.styled';
 import { getContacts, getFilter } from '../../redux/selectors';
 
-const ContactsList = ({ contacts, filter, onDeleteContact }) => {
+const ContactsList = () => {
+  const contacts = useSelector(getContacts);
+  const filter = useSelector(getFilter);
+
   return (
     <List>
       {contacts
         .filter(({ name }) => name.toLowerCase().includes(filter.toLowerCase()))
         .map(({ id, name, number }) => (
-          <ContactListItem
-            key={id}
-            id={id}
-            name={name}
-            number={number}
-            onDeleteContact={onDeleteContact}
-          />
+          <ContactListItem key={id} id={id} name={name} number={number} />
         ))}
     </List>
   );
