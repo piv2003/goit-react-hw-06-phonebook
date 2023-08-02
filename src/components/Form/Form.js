b5ae22259b09618aea675a1758d2e6b62721f565
Ihor Poliakov <piv2003@gmail.com>
2023-08-02 18:25:29 +0200

    Update Form.js

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -8,6 +8,20 @@ const initialValues = {
   number: '',
 };
 
+const userSchema = object({
+  name: string()
+    .matches(
+      /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/,
+      "Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+    )
+    .required(),
+  number: string()
+    .matches(
+      /\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/,
+      'Phone number must be digits and can contain spaces, dashes, parentheses and can start with +'
+    )
+    .required(),
+});
 export default function ContactForm({ onSubmit }) {
   function handleSubmit({ name, number }, { resetForm }) {}
   return (
