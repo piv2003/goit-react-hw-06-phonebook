db024f2c91d2a73cc0a64cb72f57d08e66a49c11
Ihor Poliakov <piv2003@gmail.com>
2023-08-02 12:20:08 +0200

    fixing Form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -3,6 +3,32 @@ import { object, string } from 'yup';
 import PropTypes from 'prop-types';
 import { FormBox, InputName, InputTel, Button } from './Form.styled';
 
+const initialValues = {
+  name: '',
+  number: '',
+};
+
+export default function ContactForm({ onSubmit }) {
+  function handleSubmit({ name, number }, { resetForm }) {}
+  return (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={handleSubmit}
+      validationSchema={userSchema}
+    >
+      <FormBox autoComplete="off">
+        <label>
+          <span>Name</span>
+        </label>
+        <label>
+          <span>Tel</span>
+        </label>
+        <Button type="submit">Add contact</Button>
+      </FormBox>
+    </Formik>
+  );
+}
+
 ContactForm.propTypes = {
   onSubmit: PropTypes.func.isRequired,
 };
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import './index.css';
 import { PersistGate } from 'redux-persist/integration/react';
-import { App } from 'components/App';
+import App from 'components/App';
 import { persistor, store } from 'redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
