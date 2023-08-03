93c11f1e2979c7738b274b0e19c7fffdf12c2c82
Ihor Poliakov <piv2003@gmail.com>
2023-08-03 23:46:55 +0200

    Update store.js

---
+++ src/redux/store.js
@@ -0,0 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { contactsReducer } from './contactsSlice';
+import { filterReducer } from './filterSlice';
+import {
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
