40f9f30d7fa19f2b6880af6fd1ea0e381d5f69fe
Ihor Poliakov <piv2003@gmail.com>
2023-08-04 19:50:10 +0200

    Update contactsSlice.js

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -2,6 +2,35 @@ import { createSlice, nanoid } from '@reduxjs/toolkit';
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
+const contactsSlice = createSlice({
+  name: 'contacts',
+  initialState: {
+    contacts: [],
+  },
+  reducers: {
+    addContact: {
+      reducer(state, action) {
+        state.contacts.push(action.payload);
+      },
+      prepare(name, number) {
+        return {
+          payload: {
+            id: nanoid(10),
+            name,
+            number,
+          },
+        };
+      },
+    },
+    deleteContact(state, action) {
+      //Using the filter function to remove a contact
+      state.contacts = state.contacts.filter(
+        contact => contact.id !== action.payload
+      );
+    },
+  },
+});
+
 const persistConfig = {
   key: 'contacts',
   storage,
